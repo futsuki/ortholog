08eae5ad4b73483d39420aaa58ff15adf0e2b6d1
futsuki <gcc111014@gmail.com>
2026-07-02 08:53:23 +0900

    docs: READMEを追加し管理者パスワードを更新する

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('SITE_TITLE', 'Simple Blog');
 define('SITE_URL', 'http://localhost');
-define('ADMIN_PASSWORD', '$2y$10$Fs3mIvhDnQi/nwxrO5y9/.rdn39aZBFaJ.rjD8jlAVS2h/iCtMwhC'); // password
+define('ADMIN_PASSWORD', '$2y$10$nRIwiz2LlaRFHvw6nJfft.a2j4mjonpz6PbYcu8oaX2HfAOzlp9nK'); // password
 define('ARTICLES_DIR', __DIR__ . '/articles');
 define('CACHE_DIR', __DIR__ . '/cache');
 define('ITEMS_PER_PAGE', 10);
